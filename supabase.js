354447bae568b721d007d932086c6cd00f9a85ed
Deepthika10 <deepthikathota@gmail.com>
2025-07-26 21:11:30 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // js/supabase-client.js
-const SUPABASE_URL = 'https://xzeehkfhltorxouqnfam.supabase.co'; // Paste your URL here
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6ZWVoa2ZobHRvcnhvdXFuZmFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MjIxMTIsImV4cCI6MjA2OTA5ODExMn0.pa0Ab7Rhpz1_b7tVvUygocRf1yVji9l143A8rGBqYLw'; // Paste your anon key here
+const SUPABASE_URL = 'https://mnhiqnqolkhlurslsiwj.supabase.co'; // Paste your URL here
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uaGlxbnFvbGtobHVyc2xzaXdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NDMzODUsImV4cCI6MjA2OTExOTM4NX0.rLM_V1i8i51wQWyuFF2tQr8fz9OVJWCU504iEiKTT5k'; // Paste your anon key here
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);

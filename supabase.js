ce9f023fdc3eb875642abb34536cbbc5b1372ccb
Deepthika10 <deepthikathota@gmail.com>
2025-07-26 15:09:09 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // js/supabase-client.js
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'; // Paste your URL here
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // Paste your anon key here
+const SUPABASE_URL = 'https://xzeehkfhltorxouqnfam.supabase.co'; // Paste your URL here
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6ZWVoa2ZobHRvcnhvdXFuZmFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MjIxMTIsImV4cCI6MjA2OTA5ODExMn0.pa0Ab7Rhpz1_b7tVvUygocRf1yVji9l143A8rGBqYLw'; // Paste your anon key here
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);

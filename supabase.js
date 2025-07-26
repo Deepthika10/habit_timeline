08fb496a34ed720dfebb97462f4a6bf469cb1b17
Deepthika10 <deepthikathota@gmail.com>
2025-07-26 14:25:11 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// js/supabase-client.js
+const SUPABASE_URL = 'YOUR_SUPABASE_URL'; // Paste your URL here
+const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY'; // Paste your anon key here
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
